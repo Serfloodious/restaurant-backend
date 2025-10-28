6edfb41afe82b73dec361dd5b1447d2e511bfaa3
Theerawat Sirisawat <sealactuary128@gmail.com>
2025-10-28 11:45:23 +0700

    Create Controller Methods

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,26 +1,14 @@
 const express = require('express');
+const {getRestaurants, getRestaurant, createRestaurant, updateRestaurant, deleteRestaurant} = require('../controllers/restaurants');
+
 const router = express.Router();
 
-const app = express();
-
-router.get('/', (req, res) => {
-    res.status(200).json({success: true, msg: 'Show all restaurants'});
-});
-
-router.get('/:id', (req, res) => {
-    res.status(200).json({success: true, msg: `Show restaurant with ID: ${req.params.id}`});
-});
-
-router.post('/', (req, res) => {
-    res.status(201).json({success: true, msg: 'Create new restaurant'});
-});
-
-router.put('/:id', (req, res) => {
-    res.status(200).json({success: true, msg: `Update restaurant with ID: ${req.params.id}`});
-});
-
-router.delete('/:id', (req, res) => {
-    res.status(200).json({success: true, msg: `Delete restaurant with ID: ${req.params.id}`});
-});
+router.route('/')
+    .get(getRestaurants)
+    .post(createRestaurant);
+router.route('/:id')
+    .get(getRestaurant)
+    .put(updateRestaurant)
+    .delete(deleteRestaurant);
 
 module.exports = router;
