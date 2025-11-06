bbc2de36d1dcc1846808bd691dff724d98dcf684
Theerawat Sirisawat <sealactuary128@gmail.com>
2025-11-06 18:27:56 +0700

    Add Restaurant model with schema definition

---
+++ models/Restaurant.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const RestaurantSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name'],
+        unique: true,
+        trim: true,
+        maxlength: [100, 'Name cannot be more than 100 characters']
+    },
+    address: {
+        type: String,
+        required: [true, 'Please add an address']
+    },
+    district: {
+        type: String,
+        required: [true, 'Please add a district']
+    },
+    province: {
+        type: String,
+        required: [true, 'Please add a province']
+    },
+    postalcode: {
+        type: String,
+        required: [true, 'Please add a postalcode'],
+        maxlength: [5, 'Postalcode cannot be more than 5 characters']
+    },
+    phone: {
+        type: String
+    },
+    openinghours: {
+        type: String,
+        required: [true, 'Please add opening hours']
+    },
+    closinghours: {
+        type: String,
+        required: [true, 'Please add closing hours']
+    }
+});
+
+module.exports = mongoose.model('Restaurant', RestaurantSchema);
