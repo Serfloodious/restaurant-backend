69b269119940bccbebe3ac2387123b43d9be8175
Theerawat Sirisawat <sealactuary128@gmail.com>
2025-11-06 19:14:03 +0700

    Implement user registration with password encryption and error handling

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,5 +4,21 @@ const User = require('../models/User');
 // @route   POST /api/v1/auth/register
 // @access  Public
 exports.register = async (req, res, next) => {
-    res.status(200).json({success: true});
+    try {
+        const { firstname, lastname, email, password, role } = req.body;
+
+        // Create user
+        const user = await User.create({
+            firstname,
+            lastname,
+            email,
+            password,
+            role
+        });
+
+        res.status(201).json({success: true});
+    } catch (err) {
+        res.status(400).json({success: false});
+        console.error(err.stack);
+    }
 };
