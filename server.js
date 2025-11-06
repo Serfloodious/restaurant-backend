a7e0ccd2cc9a53b4ac73318f25141eaef5a15b29
Theerawat Sirisawat <sealactuary128@gmail.com>
2025-11-06 16:30:10 +0700

    Connect to database with Mongoose

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// Route files
-const restaurants = require('./routes/restaurants');
+const connectDB = require('./config/db');
 
 // Load environment variables from config file
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+// Route files
+const restaurants = require('./routes/restaurants');
+
 const app = express();
+
+// Body parser
+app.use(express.json());
 
 // Mount routers
 app.use('/api/v1/restaurants', restaurants);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
