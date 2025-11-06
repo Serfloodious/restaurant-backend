603c77d1182b45602acb98fb8caf945a6c894821
Theerawat Sirisawat <sealactuary128@gmail.com>
2025-11-06 19:43:06 +0700

    Refactor authentication controller to use sendTokenResponse for token handling and add cookie parser middleware in server setup

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,10 +16,7 @@ exports.register = async (req, res, next) => {
             role
         });
 
-        // Create token
-        const token = user.getSignedJwtToken();
-
-        res.status(201).json({success: true, token});
+        sendTokenResponse(user, 200, res);
     } catch (err) {
         res.status(400).json({success: false});
         console.error(err.stack);
@@ -60,8 +57,25 @@ exports.login = async (req, res, next) => {
         });
     }
 
+    sendTokenResponse(user, 200, res);
+};
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
     // Create token
     const token = user.getSignedJwtToken();
 
-    res.status(200).json({ success: true, token });
+    const options = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    };
+
+    if (process.env.NODE_ENV === 'production') {
+        options.secure = true;
+    }
+
+    res.status(statusCode).cookie('token', token, options).json({
+        success: true,
+        token
+    });
 };
