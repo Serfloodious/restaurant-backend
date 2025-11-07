add31b0437e2d5ad93032a1e1d278ddc82ba58d0
Theerawat Sirisawat <sealactuary128@gmail.com>
2025-11-07 20:49:47 +0700

    Add logout functionality to auth controller and routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -177,3 +177,18 @@ exports.updatePassword = async (req, res, next) => {
         res.status(400).json({success: false});
     }
 };
+
+// @desc    Logout user / clear cookie
+// @route   GET /api/v1/auth/logout
+// @access  Private
+exports.logout = async (req, res, next) => {
+    res.cookie('token', 'none', {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true
+    });
+
+    res.status(200).json({
+        success: true,
+        data: {}
+    });
+};
