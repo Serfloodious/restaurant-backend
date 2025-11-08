da3c27ca563e5f4633235b45860951f550a4d80b
Theerawat Sirisawat <sealactuary128@gmail.com>
2025-11-08 15:25:29 +0700

    Add Swagger documentation for authentication and user schemas

--- routes/auth.js
+++ routes/auth.js
@@ -14,3 +14,151 @@ router.get('/logout', protect, logout);
 router.delete('/deleteaccount', protect, deleteAccount);
 
 module.exports = router;
+
+/**
+ * @swagger
+ * tags:
+ *   name: Authentication
+ *   description: Authentication and user account management
+ */
+
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     response:
+ *       200:
+ *         description: The user was successfully registered
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Some server error
+ */
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Login a user
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     response:
+ *       200:
+ *         description: The user was successfully logged in
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       400:
+ *         description: User have to provide email and password
+ *       401:
+ *         description: Password does not match or password or email cannot be converted to string
+ *       404:
+ *         description: Invalid credentials
+ *       500:
+ *         description: Some server error
+ */
+
+/**
+ * @swagger
+ * /me:
+ *   get:
+ *     summary: Get the logged in user's details
+ *     tags: [Authentication]
+ *     responses:
+ *       200:
+ *         description: The user details
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Some server error
+ */
+
+/**
+ * @swagger
+ * /updatedetails:
+ *   put:
+ *     summary: Update the logged in user's details
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/User'
+ *     response:
+ *       200:
+ *         description: The user details were successfully updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Some error happened
+ */
+
+/**
+ * @swagger
+ * /updatepassword:
+ *   put:
+ *     summary: Update the logged in user's password
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     response:
+ *       200:
+ *         description: The user password was successfully updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Some error happened
+ */
+
+/**
+ * @swagger
+ * /logout:
+ *   get:
+ *     summary: Logout the current user
+ *     tags: [Authentication]
+ *     responses:
+ *       200:
+ *         description: User logged out successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ */
+
+/**
+ * @swagger
+ * /deleteaccount:
+ *   delete:
+ *     summary: Delete the logged in user's account
+ *     tags: [Authentication]
+ *     responses:
+ *       200:
+ *         description: The user account was successfully deleted
+ */
--- routes/users.js
+++ routes/users.js
@@ -9,3 +9,57 @@ router.route('/').get(protect, authorize('admin'), getUsers);
 router.route('/:id').get(protect, authorize('admin'), getUser);
 
 module.exports = router;
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - firstname
+ *         - lastname
+ *         - email
+ *         - password
+ *       properties:
+ *         id:
+ *           type: string
+ *           format: uuid
+ *           description: The auto-generated id of the user
+ *           example: 60d0fe4f5311236168a109ca
+ *         firstname:
+ *           type: string
+ *           description: The first name of the user
+ *         lastname:
+ *           type: string
+ *           description: The last name of the user
+ *         email:
+ *           type: string
+ *           description: The email of the user
+ *         phone:
+ *           type: string
+ *           description: The contact phone number of the user
+ *         role:
+ *           type: string
+ *           description: The role of the user (e.g., user, admin)
+ *         password:
+ *           type: string
+ *           description: The password of the user
+ *       example:
+ *         id: 60d0fe4f5311236168a109ca
+ *         firstname: Jim
+ *         lastname: Doe
+ *         email: jim.d@gmail.com
+ *         phone: 123-456-7890
+ *         role: user
+ *         password: 11223344
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: User retrieval
+ */
+
+
